fcaf8632e00cd2ec2bf04a7efadef75359ca6c5f
erniranjank15 <kasoteniranjan23@gmail.com>
2026-07-26 17:02:56 +0530

    one

--- routers/config.py
+++ routers/config.py
@@ -4,6 +4,13 @@ import os
 
 load_dotenv()
 
+print("MAIL_SERVER:", os.getenv("MAIL_SERVER"))
+print("MAIL_PORT:", os.getenv("MAIL_PORT"))
+print("MAIL_STARTTLS:", os.getenv("MAIL_STARTTLS"))
+print("MAIL_SSL_TLS:", os.getenv("MAIL_SSL_TLS"))
+print("MAIL_USERNAME:", os.getenv("MAIL_USERNAME"))
+print("MAIL_PASSWORD exists:", bool(os.getenv("MAIL_PASSWORD")))
+
 conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
