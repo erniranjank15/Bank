1bc7d8b2e059c6b8d71e2518162323fcc77f9966
Er. Niranjan Kasote <kasoteniranjan23@gmail.com>
2026-07-29 17:50:37 +0530

    Update emailService.py

--- service/emailService.py
+++ service/emailService.py
@@ -1,13 +1,31 @@
-from fastapi_mail import FastMail, MessageSchema, MessageType
-from routers.config import conf
+import os
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+configuration = sib_api_v3_sdk.Configuration()
+configuration.api_key["api-key"] = os.getenv("BrevoEmailAPIKEY")
+
+api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+    sib_api_v3_sdk.ApiClient(configuration)
+)
 
 async def send_email(recipient: str, subject: str, body: str):
-    message = MessageSchema(
+    email = sib_api_v3_sdk.SendSmtpEmail(
+        to=[{"email": recipient}],
+        sender={
+            "email": os.getenv("BREVO_SENDER_EMAIL"),
+            "name": os.getenv("BREVO_SENDER_NAME"),
+        },
         subject=subject,
-        recipients=[recipient],
-        body=body,
-        subtype=MessageType.html
+        html_content=body,
     )
 
-    fm = FastMail(conf)
-    await fm.send_message(message)
+    try:
+        api_instance.send_transac_email(email)
+        print("✅ Email sent successfully")
+    except ApiException as e:
+        print("❌ Brevo Error:", e)
+        raise
